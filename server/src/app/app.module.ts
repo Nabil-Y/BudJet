a94535dba9bec569bbc32bc4c21cc0ffd653c373
nabil-y <nabil.yassine@neomi.immo>
2023-03-18 02:21:06 +0100

    add morgan logger

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,16 +1,11 @@
-import { MiddlewareConsumer, Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UserModule } from "src/user/user.module";
-import { LoggerMiddleware } from "src/middlewares/global/logger.middleware";
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes("*");
-  }
-}
+export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,25 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
 import helmet from "helmet";
+import * as morgan from "morgan";
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.enableCors();
   app.setGlobalPrefix("api/v1");
-  await app.listen(3000);
+  const httpLogger = new Logger("HTTP");
+  const appLogger = new Logger("HTNestApplicationTP");
+  app.use(
+    morgan(":method :url :status - :response-time ms", {
+      stream: {
+        write: (message) => httpLogger.log(message),
+      },
+    })
+  );
+  const port = process.env.PORT || "Port not specified";
+  await app.listen(port);
+  appLogger.log(`Application is running on port ${port}}`);
 }
 bootstrap();
